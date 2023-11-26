77df191d46e84c672b6ef061d3b77db5ac966b88
Matthew Rawson <matthewrawson2@gmail.com>
2023-11-26 03:12:01 +0000

    Add files via upload

---
+++ ONeilloApp/ONeilloApp/CustomObjects/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿namespace ONeilloApp.CustomObjects
+{
+    internal class Rectangle : Panel
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2); // Black pen with width 2
+            System.Drawing.Rectangle rect = new(0, 0, Width, Height);
+
+            graphics.DrawRectangle(pen, rect);
+        }
+    }
+}
