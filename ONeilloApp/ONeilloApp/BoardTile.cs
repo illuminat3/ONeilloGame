9e8c2cd9f87665058875638aa64a0b7da45c088c
Matthew Rawson <matthewrawson2@gmail.com>
2023-11-26 03:11:04 +0000

    Add files via upload

---
+++ ONeilloApp/ONeilloApp/BoardTile.cs
@@ -0,0 +1,19 @@
+﻿namespace ONeilloApp
+{
+    public enum PossibleValues
+    {
+        EMPTY,
+        WHITE,
+        BLACK
+    }
+
+    internal class BoardTile
+    {
+        public PossibleValues Value;
+
+        public BoardTile()
+        {
+            this.Value = PossibleValues.EMPTY;
+        }
+    }
+}
