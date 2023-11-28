edbf518f9bc8378acad8a9303b63174afa4441eb
Matthew Rawson <mrawson@redev.co.uk>
2023-11-28 15:41:09 +0000

    Fixed Issue #5

--- ONeilloApp/ONeilloApp/SelectGame.Designer.cs
+++ ONeilloApp/ONeilloApp/SelectGame.Designer.cs
@@ -32,6 +32,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "SelectGame";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
         }
 
         #endregion
